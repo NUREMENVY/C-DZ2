22c5c0dabdaa7cd0cb2d8277dfe9e7fa92a0f7c7
NUREM <NUREMX@GMAIL.COM>
2022-10-07 02:11:09 +0300

    dz2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите трехзначное число.");
+    string input = Console.ReadLine();
+    Console.WriteLine("{0}->{1}",input, input[2]);
+    if (input.Length > input [2])
+        Console.WriteLine("no");
+
+
---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("digit (day of the week)-> ");
+int day = Convert.ToInt32(Console.ReadLine());
+void x (int day) {
+  if (day == 6 || day == 7) 
+  {
+  Console.WriteLine("holiday");
+  }
+  else if (day < 1 || day > 7) 
+  {
+    Console.WriteLine("onli 7 days");
+  }
+  else Console.WriteLine("not holiday");
+}
+x(day);
+Console.WriteLine(day);
